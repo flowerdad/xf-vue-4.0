3338d260965ea875676292e6597134edc055875a
flowerdad <229084202@qq.com>
2020-05-11 18:41:03 +0800

    地图类封装

--- src/assets/js/vMap.js
+++ src/assets/js/vMap.js
@@ -20,11 +20,17 @@ let mapConfig = {
   center: [116.353897, 40.072519]
 };
 
+// 单位图层
+let buildingLayer = "";
+
 // 公共参数
 let markerBackColor = "font-color-level1";
 let markerIconColor = "back-color";
 
-// 公共函数
+/**
+ * marker 初始化icon
+ * @param {icon图标} icon
+ */
 function initMarkerIcon(icon) {
   var con =
     `
@@ -51,6 +57,60 @@ function initMarkerIcon(icon) {
 //   return false;
 // }
 
+/**
+ * 解析DrivingRoute对象，构造成AMap.Polyline的path参数需要的格式
+ * DrivingResult对象结构参考文档 https://lbs.amap.com/api/javascript-api/reference/route-search#m_DriveRoute
+ * @param {*} route
+ */
+function parseRouteToPath(route) {
+  var path = [];
+
+  for (var i = 0, l = route.steps.length; i < l; i++) {
+    var step = route.steps[i];
+
+    for (var j = 0, n = step.path.length; j < n; j++) {
+      path.push(step.path[j]);
+    }
+  }
+  return path;
+}
+
+/**
+ * 画路线
+ * @param {*} route
+ */
+function drawRoute(map, route, strokeColor = "#0091ff") {
+  var path = parseRouteToPath(route);
+
+  var startMarker = new AMap.Marker({
+    position: path[0],
+    icon: "https://webapi.amap.com/theme/v1.3/markers/n/start.png",
+    map: map
+  });
+
+  var endMarker = new AMap.Marker({
+    position: path[path.length - 1],
+    icon: "https://webapi.amap.com/theme/v1.3/markers/n/end.png",
+    map: map
+  });
+
+  var routeLine = new AMap.Polyline({
+    path: path,
+    isOutline: true,
+    outlineColor: "#525252",
+    borderWeight: 1,
+    strokeWeight: 5,
+    strokeColor: strokeColor,
+    lineJoin: "round"
+  });
+
+  routeLine.setMap(map);
+
+  // 调整视野达到最佳显示区域
+  map.setFitView([startMarker, endMarker, routeLine]);
+  return path;
+}
+
 // 全局map函数
 let map = {
   // 初始化map地图
@@ -70,6 +130,40 @@ let map = {
     // 将地图对象存入vuex
     store.commit("map", map);
     return map;
+  },
+
+  /**
+   * 计算区域中心点
+   * @param {区域数组} lnglatarr
+   */
+  calculateCenter(lnglatarr) {
+    console.log(lnglatarr);
+    var total = lnglatarr.length;
+    let X = 0,
+      Y = 0,
+      Z = 0;
+
+    lnglatarr.forEach(element => {
+      var lng = (element.lng * Math.PI) / 180;
+      var lat = (element.lat * Math.PI) / 180;
+      var x, y, z;
+      x = Math.cos(lat) * Math.cos(lng);
+      y = Math.cos(lat) * Math.sin(lng);
+      z = Math.sin(lat);
+      X += x;
+      Y += y;
+      Z += z;
+    });
+
+    X = X / total;
+    Y = Y / total;
+    Z = Z / total;
+
+    var Lng = Math.atan2(Y, X);
+    var Hyp = Math.sqrt(X * X + Y * Y);
+    var Lat = Math.atan2(Z, Hyp);
+
+    return new AMap.LngLat((Lng * 180) / Math.PI, (Lat * 180) / Math.PI);
   },
 
   /**
@@ -255,9 +349,7 @@ let map = {
   /**
    * polyEditor 编辑单位函数
    * @param {*} position 初始坐标
-   *
-   * @returns [polygon, polyEditor]
-   * polygon:矢量图
+   * @returns polyEditor
    * polyEditor：编辑工具obj
    */
   polyEditor(obj) {
@@ -266,6 +358,7 @@ let map = {
     let position = obj.position.split(","),
       lng = parseFloat(position[0]),
       lat = parseFloat(position[1]),
+      // 经纬基数
       base = 0.0009,
       newPostion = [
         [lng - base, lat + base],
@@ -289,11 +382,15 @@ let map = {
     var polyEditor = new AMap.PolyEditor(store.state.map, polygon);
     polyEditor.open();
     // 返回矢量图形，及编辑工具obj。
-    return [polygon, polyEditor];
+    return polyEditor;
   },
 
+  /**
+   * 加载楼块
+   * @param {*} areas 楼块对象
+   */
   createUnitArea(areas) {
-    let buildingLayer = new AMap.Buildings({
+    buildingLayer = new AMap.Buildings({
       zIndex: 130,
       merge: false,
       sort: false,
@@ -309,6 +406,85 @@ let map = {
     //此配色优先级高于自定义mapStyle
     buildingLayer.setStyle(options);
     store.state.map.setLayers([new AMap.TileLayer(), buildingLayer]);
+
+    // 画范围及标识
+    areas.forEach(element => {
+      let polygon = new AMap.Polygon({
+        path: element.path,
+        strokeColor: "#000000",
+        strokeWeight: 2,
+        strokeOpacity: 0.2,
+        fillOpacity: 0.5,
+        fillColor: element.color,
+        zIndex: 50
+      });
+      store.state.map.add(polygon);
+      var obj = {
+        type: element.type,
+        id: element.id,
+        x: element.center.lng,
+        y: element.center.lat,
+        icon: "el-icon-star-on"
+      };
+      this.addMarker(obj);
+    });
+  },
+
+  /**
+   * 路线规划
+   * @param {地图对象} map
+   * @param {开始坐标} star
+   * @param {结束坐标} end
+   */
+  drivingPolicy(map, opt, callback) {
+    var drivingOption = {
+      policy: AMap.DrivingPolicy.LEAST_TIME // 其它policy参数请参考 https://lbs.amap.com/api/javascript-api/reference/route-search#m_DrivingPolicy
+    };
+
+    // 构造路线导航类
+    var driving = new AMap.Driving(drivingOption);
+
+    // 根据起终点经纬度规划驾车导航路线
+    driving.search(
+      new AMap.LngLat(opt.star.lng, opt.star.lat),
+      new AMap.LngLat(opt.end.lng, opt.end.lat),
+      function (status, result) {
+        // result即是对应的驾车导航信息，相关数据结构文档请参考 https://lbs.amap.com/api/javascript-api/reference/route-search#m_DrivingResult
+        if (status === "complete") {
+          if (result.routes && result.routes.length) {
+            // 绘制第一条路线，也可以按需求绘制其它几条路线
+            callback(drawRoute(map, result.routes[0], opt.color));
+          }
+        } else {
+          console.log("获取驾车数据失败：" + result);
+        }
+      }
+    );
+  },
+  drivTrajectory(map, position) {
+    // 汽车对象
+    let marker = new AMap.Marker({
+      map: map,
+      position: position,
+      icon: "https://webapi.amap.com/images/car.png",
+      offset: new AMap.Pixel(-26, -13),
+      autoRotation: true,
+      angle: -90
+    });
+    // 绘制轨迹
+    var passedPolyline = new AMap.Polyline({
+      map: map,
+      // path: lineArr,
+      strokeColor: "#AF5", //线颜色
+      // strokeOpacity: 1,     //线透明度
+      strokeWeight: 6 //线宽
+      // strokeStyle: "solid"  //线样式
+    });
+
+    marker.on("moving", function (e) {
+      passedPolyline.setPath(e.passedPath);
+    });
+    return marker;
   }
 };
 
--- src/components/card/test/card3.vue
+++ src/components/card/test/card3.vue
@@ -38,6 +38,16 @@
           </el-col>
         </el-row>
         <el-divider></el-divider>
+        <el-button type="primary" @click="getUnit()" size="mini">加载已有单位</el-button>
+        <div class="font-color size-12" v-if="unitShow">
+          <p v-for="unit in unitList" :key="unit.id" class="markerItem">
+            unit{{ unit.id }}
+            <el-tag size="mini" class="tag" @click="selMarker(unit.type, unit.id)">定位</el-tag>
+            <el-tag size="mini" class="tag" @click="drivingPolicy(unit)">路线规划</el-tag>
+            <el-tag size="mini" class="tag" @click="drivTrajectory(unit)">轨迹</el-tag>
+          </p>
+        </div>
+        <el-divider></el-divider>
       </div>
     </cardBlock>
   </div>
@@ -59,7 +69,91 @@ export default {
         areaColor: "#409EFF"
       },
       unitAreaPath: [],
-      areas: []
+      areas: [],
+      unitShow: false,
+      unitList: [
+        {
+          id: 1,
+          type: "unit",
+          color: "#409EFF",
+          color1: "E5409EFF",
+          color2: "E579bbff",
+          center: {
+            Q: 40.073375277695646,
+            R: 116.34752349893068,
+            lng: 116.347523,
+            lat: 40.073375
+          },
+          path: [
+            [116.345049, 40.075559],
+            [116.344923, 40.071234],
+            [116.350006, 40.071222],
+            [116.350116, 40.075486]
+          ]
+        },
+        {
+          id: 2,
+          type: "unit",
+          color: "#40FF43",
+          color1: "E540FF43",
+          color2: "E579ff7b",
+          center: {
+            Q: 40.07424752528499,
+            R: 116.32706001522553,
+            lng: 116.32706,
+            lat: 40.074248
+          },
+          path: [
+            [116.32378, 40.0762],
+            [116.325753, 40.07146],
+            [116.329873, 40.071487],
+            [116.328834, 40.077843]
+          ]
+        },
+        {
+          id: 3,
+          type: "unit",
+          color: "#FCFF40",
+          color1: "E5FCFF40",
+          color2: "E5fcff79",
+          center: {
+            Q: 40.06383717939862,
+            R: 116.3619052985332,
+            lng: 116.361905,
+            lat: 40.063837
+          },
+          path: [
+            [116.36096, 40.06606],
+            [116.357333, 40.065582],
+            [116.35869, 40.06238],
+            [116.361638, 40.06197],
+            [116.364586, 40.060992],
+            [116.365331, 40.063908],
+            [116.364799, 40.065968]
+          ]
+        },
+        {
+          id: 4,
+          type: "unit",
+          color: "#FF6040",
+          color1: "E5FF6040",
+          color2: "E5ff8f79",
+          center: {
+            Q: 40.065787543071025,
+            R: 116.33824901385486,
+            lng: 116.338249,
+            lat: 40.065788
+          },
+          path: [
+            [116.334753, 40.068307],
+            [116.334805, 40.065484],
+            [116.337277, 40.065348],
+            [116.33781, 40.063707],
+            [116.34254, 40.06373],
+            [116.342309, 40.068149]
+          ]
+        }
+      ]
     };
   },
   methods: {
@@ -111,11 +205,11 @@ export default {
     },
     polyEditor(obj) {
       // unitArea详细信息，请看polyEditor注释。
-      let unitArea = this.vMap.map.polyEditor(obj);
-      this.unitArea = unitArea[1];
+      this.unitArea = this.vMap.map.polyEditor(obj);
       this.unitArea.on("end", event => {
         // 组装path
         event.target.w.path.forEach(element => {
+          console.log(element.lng);
           this.unitAreaPath.push([element.lng, element.lat]);
         });
         // 组装颜色
@@ -125,24 +219,56 @@ export default {
           10,
           "16"
         );
+        // 组装数据格式
         this.areas.push({
+          id: 0,
+          type: "unit",
+          color: obj.areaColor,
           color1: this.vTools.tools.color16ToOpacity16(obj.areaColor),
           color2: this.vTools.tools.color16ToOpacity16(colorScale[3]),
+          center: this.vMap.map.calculateCenter(event.target.w.path),
           path: this.unitAreaPath
         });
         // 画区块
         this.vMap.map.createUnitArea(this.areas);
-        // 画单位范围
-        unitArea[0].setOptions({
-          fillColor: obj.areaColor,
-          fillOpacity: 0.5
-        });
       });
+    },
+    getUnit() {
+      this.unitShow = true;
+      this.vMap.map.createUnitArea(this.unitList);
     },
     endUnitArea() {
       this.unitArea.close();
       this.$store.state.map.setRotation(this.vMap.mapConfig.rotation);
       this.$store.state.map.setPitch(this.vMap.mapConfig.pitch);
+    },
+    drivingPolicy(unit) {
+      this.vMap.map.drivingPolicy(
+        this.$store.state.map,
+        {
+          star: unit.center,
+          end: {
+            lng: 116.347187,
+            lat: 40.067107
+          },
+          color: unit.color
+        },
+        e => {
+          this.unitList.forEach(element => {
+            if (unit.id == element.id) {
+              element.paths = e;
+            }
+          });
+        }
+      );
+    },
+    drivTrajectory(unit) {
+      console.log(this.unitList);
+      let trajectory = this.vMap.map.drivTrajectory(this.$store.state.map, [
+        unit.center.lng,
+        unit.center.lat
+      ]);
+      trajectory.moveAlong(unit.paths, 200);
     }
   }
 };
